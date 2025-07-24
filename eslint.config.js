3410b7320f24f0062a07ef9fdc566243e38092be
Yuki <yuki2082710@gmail.com>
2025-07-24 15:50:18 +0900

    Integrate eslint-plugin-unused-imports, disable unused vars rule, fix BlogList destructuring

--- eslint.config.js
+++ eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import { globalIgnores } from "eslint/config";
+import unusedImports from "eslint-plugin-unused-imports";
 
 export default tseslint.config([
 	globalIgnores(["dist", "scripts"]),
@@ -16,9 +17,25 @@ export default tseslint.config([
 			reactHooks.configs["recommended-latest"],
 			reactRefresh.configs.vite,
 		],
+		plugins: {
+			"unused-imports": unusedImports,
+		},
 		languageOptions: {
 			ecmaVersion: 2020,
 			globals: globals.browser,
+		},
+		rules: {
+			"no-unused-vars": "off",
+			"unused-imports/no-unused-imports": "error",
+			"unused-imports/no-unused-vars": [
+				"warn",
+				{
+					vars: "all",
+					varsIgnorePattern: "^_",
+					args: "after-used",
+					argsIgnorePattern: "^_",
+				},
+			],
 		},
 	},
 ]);
